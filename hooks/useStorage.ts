12b8a70bf65c397984a4e1b06a50df70c4c7c8da
somsak.mun <somsak.mun@gfinthailand.com>
2024-07-26 22:26:12 +0700

    Rename hooks_temp to hooks

---
+++ hooks/useStorage.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+type TInputValue = string | number | boolean | object | undefined | Array<any>
+export const useSaveData = async <T extends TInputValue>(keyName: string, inputValue: T): Promise<void> => {
+  try {
+    await AsyncStorage.setItem(keyName, JSON.stringify(inputValue))
+  } catch (error) {
+    console.error('Error saving data', error)
+  }
+}
+
+export const useGetTasks = async (): Promise<TaskType[]> => {
+  return AsyncStorage.getItem(`tasks`)
+    .then((json) => {
+      return (JSON.parse(json || '[]') as TaskType[])
+    })
+}
